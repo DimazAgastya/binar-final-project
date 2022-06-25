cbb36facd063c7ee1e68016548cace7963e114f8
rifdaazkya <100396878+rifdaazkya@users.noreply.github.com>
2022-06-25 14:43:13 +0700

    commit pages buyer, seller (info produk), info penawaran

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import tes from './tes'
 
-function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navbar from './Components/Navbar/EarlyNavbar';
+import Buyer from './Pages/Buyer'
+import InfoPenawaran from './Pages/InfoPenawaran';
+import Seller from './Pages/Seller'
+
+const App = ()  => {
+
   return (
-    <div className="App">
-    <tes/>
-    </div>
+   <div className="app_container">
+    <BrowserRouter>
+     <Routes>
+       <Route path ="/" element={<Buyer/>}/>
+       <Route path ="/infopenawaran" element={<InfoPenawaran/>}/>
+       <Route path ="/seller" element={<Seller/>}/>
+     </Routes>
+     </BrowserRouter>
+   </div>
   );
 }
 
--- src/tes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function tes() {
-  return (
-    <h1>tes</h1>
-  );
-}
-
-export default tes;
